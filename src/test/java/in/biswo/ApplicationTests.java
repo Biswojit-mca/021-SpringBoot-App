420dc58c71fd1fdbd30022a3f9c53c6ce6f79664
Biswojit Das <biswojit501811@gmail.com>
2022-08-16 15:07:47 +0530

    initial commit

---
+++ src/test/java/in/biswo/ApplicationTests.java
@@ -0,0 +1,13 @@
+package in.biswo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
