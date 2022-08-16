fccd87ad9d778c202893a9e5aae7e575d0809960
Biswojit Das <biswojit501811@gmail.com>
2022-08-16 16:57:42 +0530

    HIS-134 fix provided

--- src/main/java/in/biswo/Application.java
+++ src/main/java/in/biswo/Application.java
@@ -9,7 +9,8 @@ public class Application {
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext run = SpringApplication.run(Application.class, args);
-	    run.close();
+	    //changes for HIS-134
+		run.close();
 	
 	}
 
